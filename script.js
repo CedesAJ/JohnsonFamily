5e1c775368771ddb7f6f566b04036b549aea6750
CedesAJ <76976287+CedesAJ@users.noreply.github.com>
2026-07-28 17:17:15 -0500

    Refactor birthday fetching and display logic

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const menuButton = document.querySelector(".menu-toggle");
-const nav = document.querySelector(".navbar");
-
-menuButton.addEventListener("click", () => {
-    nav.classList.toggle("active");
-});
-
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 
 const container = document.getElementById("birthdayContainer");
 
