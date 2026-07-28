cb9f01ba00451f0fbb9e0e632a2f248b265d1757
CedesAJ <76976287+CedesAJ@users.noreply.github.com>
2026-07-28 17:55:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+  document.getElementById("myTopnav").classList.toggle("responsive");
 
 const container = document.getElementById("birthdayContainer");
 
